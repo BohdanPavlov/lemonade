cf9b67cc9c926110dee73ed3bcb1af9ebabbd601
BohdanPavlov <bogdanpavlov77@gmail.com>
2021-11-29 15:39:17 +0200

    homework 4 done

--- lesson4/js/homework.js
+++ lesson4/js/homework.js
@@ -1,16 +1,23 @@
 //1
-//let arr = [1,33,456,986,122]
 //найти суму квадратов всех значений масива (reduce)
+let arr = [1,33,456,986,122]
+let reduceArr = arr.reduce(function (previousValue,item){
+    return item**2 + previousValue;
+},0);
+console.log(reduceArr)
 //2
-//let arr = [ 'Tanita', 'Vasyl', 'George', 'Stepan']
 //получить массив вида: [ 'My name is Tanita','My name is Vasyl', и тд... ] использовать map
+let arr2 = [ 'Tanita', 'Vasyl', 'George', 'Stepan']
+let myName = arr2.map(item => `My name is ${item}`)
+console.log(myName)
 //3
 //написать функцию которая будет принимать входным параметром массив
 //и переставит в нем елементы так что в начале массива будут только цифры а в конце только строки
 //
-//let arr =[2, 6, 7, "a", "b", "q"]
-// function sortTheArray(someArray){
-//     // ваш код
-// }
-//
-// sortTheArray(["b", 6, "a", "q", 7, 2])
+function sortTheArray(someArray){
+    let newArray = someArray.sort()
+    console.log(newArray)
+    let sortedArray = newArray.sort((a, b) => a-b)
+    console.log(sortedArray)
+}
+sortTheArray(["b", 6, "a", "q", 7, 2, 15, 25, 'Oleg', 'Petro'])
